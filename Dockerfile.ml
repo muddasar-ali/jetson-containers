f6de2a1948b214f3132951fa35d3cc0329dd2357
muddasar-ali <68741549+muddasar-ali@users.noreply.github.com>
2021-06-21 13:05:14 -0500

    Updated Dockerfile.ml
    Added GST-RSTP-SERVER

--- Dockerfile.ml
+++ Dockerfile.ml
@@ -176,7 +176,7 @@ RUN git clone https://github.com/muddasar-ali/tensorrtx.git && \
 #GSTREAMER INSTALLATION WITH GST-RSTP-SERVER
 #
 
-RUN apt-get install libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio && \
+RUN apt-get install -y libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio && \
         git clone git://anongit.freedesktop.org/gstreamer/gst-rtsp-server && \
         cd gst-rtsp-server && \
         git checkout remotes/origin/1.2 && \
